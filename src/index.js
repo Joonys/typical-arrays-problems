96c0efd512fc829e6fc9dca7364f5423e1d8f3e9
Joonys <yurii.kletsko@gmail.com>
2020-02-23 22:10:26 +0200

    complited: Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array = []) {
+  return (array.length > 0) ? Math.min(...array) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = []) {
+  return (array.length > 0) ? Math.max(...array) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = []) {
+  const result = (array.length > 0) ?
+    array.reduce((sum, cur) => (sum + cur), 0) / array.length
+    : 0;
+
+  return result.toFixed(2)
 }
